6f6bb872c4f08361d11ef3fae5bd7010c1318e67
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2023-07-08 23:56:39 +1000

    change cart to singleton

--- module/Square/src/Square/Factory/Cart.php
+++ module/Square/src/Square/Factory/Cart.php
@@ -4,7 +4,21 @@ namespace Square\Factory;
 
 class Cart
 {
+    private static $instance;
     private $items = [];
+
+    private function __construct()
+    {
+        // Private constructor to prevent direct instantiation
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Cart();
+        }
+        return self::$instance;
+    }
 
     public function addToCart($item)
     {
